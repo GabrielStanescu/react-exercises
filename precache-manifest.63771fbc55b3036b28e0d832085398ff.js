a47af01ab1867e0e02c1ce724ee1f0a896f655d9
GabrielStanescu <stanescu.gabriel2001@yahoo.com>
2022-05-23 20:43:07 +0300

    Updates

---
+++ precache-manifest.63771fbc55b3036b28e0d832085398ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f93b4e77f5c4daf16c21",
+    "url": "/react-exercises/static/css/main.35e4b264.chunk.css"
+  },
+  {
+    "revision": "f93b4e77f5c4daf16c21",
+    "url": "/react-exercises/static/js/main.3cc1eefe.chunk.js"
+  },
+  {
+    "revision": "954d27109b9a90aad215",
+    "url": "/react-exercises/static/js/runtime~main.454a781a.js"
+  },
+  {
+    "revision": "9e56cdc7493afb21c16d",
+    "url": "/react-exercises/static/js/2.82e42d58.chunk.js"
+  },
+  {
+    "revision": "2ded26446bc94b81347a",
+    "url": "/react-exercises/static/js/3.398cb210.chunk.js"
+  },
+  {
+    "revision": "48374c2f4caa45156eca6743516dbd44",
+    "url": "/react-exercises/index.html"
+  }
+];
